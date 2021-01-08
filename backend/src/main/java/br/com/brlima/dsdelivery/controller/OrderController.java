54364dd5037371f1a24125918c3c3cb7e519feb9
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-01-08 13:43:06 -0300

    feat: add endpoint to insert order

---
+++ backend/src/main/java/br/com/brlima/dsdelivery/controller/OrderController.java
@@ -0,0 +1,36 @@
+package br.com.brlima.dsdelivery.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.brlima.dsdelivery.service.OrderService;
+import br.com.brlima.dsdelivery.service.dto.OrderDTO;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll() {
+        var result = service.findAll();
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        var newOrder = service.insert(dto);
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newOrder.getId()).toUri();
+        return ResponseEntity.created(uri).body(newOrder);
+    }
+}
---
+++ backend/src/main/java/br/com/brlima/dsdelivery/service/OrderService.java
@@ -0,0 +1,45 @@
+package br.com.brlima.dsdelivery.service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.brlima.dsdelivery.entity.Order;
+import br.com.brlima.dsdelivery.entity.enums.OrderStatus;
+import br.com.brlima.dsdelivery.repository.OrderRepository;
+import br.com.brlima.dsdelivery.repository.ProductRepository;
+import br.com.brlima.dsdelivery.service.dto.OrderDTO;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		var result = repository.findOrdersWithProducts();
+		return result.stream().map(OrderDTO::new).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		final var order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING, null);
+
+		dto.getProducts().forEach(p -> {
+			var product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		});
+
+		var newOrder = repository.save(order);
+		return new OrderDTO(newOrder);
+	}
+}
