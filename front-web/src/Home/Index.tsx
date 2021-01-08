ca5715df764736243fb37bac1b0145aaadd402a8
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-01-08 19:21:55 -0300

    feat: change tag a to Link

--- front-web/src/Home/Index.tsx
+++ front-web/src/Home/Index.tsx
@@ -1,6 +1,7 @@
 import './Styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer/Index';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -10,15 +11,15 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">
                             Faça o seu pedido <br /> Que entregamos <br /> pra você!!!
-                    </h1>
+                        </h1>
 
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta
-                    </h3>
+                        </h3>
 
-                        <a href="orders" className="home-btn-order">
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
 
                     <div className="home-image">
