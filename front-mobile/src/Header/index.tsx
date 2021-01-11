b9566e440e5b6c6269fc3cadfb3b8ff620dace2a
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-01-11 13:16:03 -0300

    feat: add Header and Home in App

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,19 +1,7 @@
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
-import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-    let [fontsLoaded] = useFonts({
-        OpenSans_400Regular,
-        OpenSans_700Bold
-    });
-
-    if (!fontsLoaded) {
-        return <AppLoading />
-    }
-
     return (
         <View style={styles.container}>
             <Image source={require('../../assets/logo.png')} />
